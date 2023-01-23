1449e5a1a7a6488f277487a04a02510a4577f170
bio-boris <bio-boris@users.noreply.github.com>
2023-01-22 22:38:15 -0600

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -7,4 +7,17 @@ if [ "$CONDOR_JWT_TOKEN" ] ; then
      chmod 600 /home/restd/.condor/tokens.d/JWT
 fi
 
-gunicorn -w 4 -b 127.0.0.1:5000 condor_restd:app --access-logfile -
+# Set the number of gevent workers to number of cores * 2 + 1
+# See: http://docs.gunicorn.org/en/stable/design.html#how-many-workers
+calc_workers="$(($(nproc) * 2 + 1))"
+# Use the WORKERS environment variable, if present
+workers=${WORKERS:-$calc_workers}
+
+
+gunicorn \
+  --access-logfile - \
+  --timeout 1800 \
+  --workers $workers \
+  --bind :5000 \
+  ${DEVELOPMENT:+"--reload"} \
+  condor_restd:app
